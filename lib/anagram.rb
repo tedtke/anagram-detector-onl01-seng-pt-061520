cebe194578f2fe49d895ecc2a0324bbc70d2aa3b
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-13 17:31:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(some_word)
+    some_word.select{|w| w.split("").sort == word.split("").sort}
+  end
+      
+
+end
